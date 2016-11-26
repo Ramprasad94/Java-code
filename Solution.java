40a05ec5282cbc73c208ee1db695d341f945c6da
Ramprasad Bommaganty <rbommaga@blesmol.soic.indiana.edu>
2016-11-26 16:51:38 -0500

    new changes to solution.java

--- Solution.java
+++ Solution.java
@@ -9,31 +9,29 @@ public class Solution {
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
      Scanner sc = new Scanner(System.in);
-     String[] inputStringArray = new String[sc.nextInt()]; 
-     
-     for (int i =0; i < inputStringArray.length; i++){ //every String is taken up depending on T
-     String inputString = sc.next();
-     char[] inputStringToChar = inputString.toCharArray();
-     int k = 0, l = 0;
-     char[] tempOddArray = new char[inputStringToChar.length];
-     char[] tempEvenArray = new char[inputStringToChar.length];
-	 for(int j = 0 ; j< inputStringToChar.length; j++){
-             if(j%2==0){
-              tempEvenArray[k]=inputStringToChar[j];         
-             k++;
-             }
-            else{
-             tempOddArray[l]= inputStringToChar[j];   
-				l++;
-            }
+     int testCaseCount = sc.nextInt();
+     String[] inputStringArray = new String[testCaseCount];
+     for(int count = 0; count < testCaseCount; count++){
+		 inputStringArray[count] = sc.next();
 	 }
-     String oddString = new String(tempOddArray);
-     String evenString = new String(tempEvenArray);
-     System.out.print(evenString);   
-     System.out.print(" ");
-     System.out.print(oddString);   
-     System.out.println();
-     }
-    sc.close();
-    }
+     StringBuilder evenBuild = new StringBuilder();
+     StringBuilder oddBuild = new StringBuilder();
+     for(int i = 0; i < inputStringArray.length; i++){
+		String inputString = inputStringArray[i];
+		for(int j = 0 ; j < inputString.length(); j++){
+			if(j%2 == 0){
+				evenBuild.append(inputString.charAt(j));
+				}
+			else{
+				oddBuild.append(inputString.charAt(j));
+				}
+			}
+		System.out.print(evenBuild.toString());
+		System.out.print(" ");
+		System.out.print(oddBuild.toString());
+		System.out.println();
+		evenBuild.delete(0, evenBuild.length());
+		oddBuild.delete(0, oddBuild.length());
+	}
+}
 }
