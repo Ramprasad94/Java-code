9dd4e2f2865dab4e0f04e963d60138fd629502a6
Ramprasad Bommaganty <rbommaga@indiana.edu>
2017-02-27 20:13:12 -0500

    dtob

---
+++ DecimalToBinary.java
@@ -0,0 +1,26 @@
+//Program converts decimal number to a binary number
+import java.io.*;
+import java.util.*;
+
+public class DecimalToBinary{
+	
+	public static void main(String args[]){
+
+			int binary = 0, i = 1;
+			System.out.println("Enter the decimal number");
+			Scanner sc = new Scanner(System.in);
+			int number = sc.nextInt();
+			if(number < 0){
+				System.out.println("Enter a number that is greater than 0");
+			}
+			else{
+				while(number != 0){
+				int remainder = number%2;
+				number = number/2;
+				binary = binary + (remainder*i); //makes a new addition only when rem is non-zero
+				i = i * 10;
+				}
+			}
+			System.out.println(binary);
+	}
+}
