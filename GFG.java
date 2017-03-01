213bb63e10a2c7e5206d7d0d2240d125d892a77a
Ramprasad Bommaganty <rbommaga@indiana.edu>
2017-02-28 21:44:25 -0500

    improved GFG file, with formatting

--- GFG.java
+++ GFG.java
@@ -5,7 +5,7 @@ import java.util.*;
 
 public class GFG{
 	public static void main(String args[]){
-		int final_result = 0;
+		long final_result = 0;
 		System.out.println("Enter n:");
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
@@ -19,34 +19,43 @@ public class GFG{
 			binary_n = binary_n + (remainder*i);
 			i = i * 10;
 		}
-		System.out.println("The binary representation is: "+binary_n);
-		String n_string = Integer.toString(binary_n);
-		if(n_string.charAt(k+1) == '1'){
+		
+		
+		String n_string = String.format("%08d", binary_n);
+		System.out.println("The binary representation is: "+n_string);
+		if(k <= n_string.length()){
+		if(n_string.charAt(k-1) == '1'){
 			System.out.println("1 has been found");
 			char[] n_charArray = n_string.toCharArray();
 			n_charArray[k-1] = '0';
 			String temp = new String(n_charArray);
+			System.out.println("Temp is: "+ temp);
 			final_result = Integer.parseInt(temp);
-			System.out.println("The final output is: "+ final_result);
-		}
-		else{
-			System.out.println("1 has not been found");
-		}
+		
 	
 		//converting the decimal back to binary
-
-		System.out.println("Given binary representation: "+final_result);
-		int temp_val = final_result;
-		int final_val = 0;
+		System.out.print("Given binary representation: ");
+		System.out.format("%08d",final_result);
+		long temp_val = final_result;
+		long final_val = 0;
 		int iterator = 0;
 		while(temp_val != 0){
-			int new_rem = temp_val%10;
+			int new_rem = (int)temp_val%10;
 			final_val = final_val+(int)(Math.pow(2,iterator) * new_rem);
 			iterator++;
 			temp_val = temp_val/10;
+			}
+		System.out.println("");
+		System.out.println("The final value is: "+final_val);
+			}
+		else{
+			System.out.println("1 has not been found");
+			}
+		
 		}
-		System.out.println(final_val);
-
-
+		else{
+			System.out.println("k value was larger than the binary representation of n");
+		}
+		
 	}
 }
