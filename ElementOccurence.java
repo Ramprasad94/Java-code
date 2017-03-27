1d699fd80029b9c32ed9570f694e276e93f5012e
Ramprasad Bommaganty <bommagantiramprasad@yahoo.com>
2017-03-27 16:44:48 -0400

    comment added to ElementOccurence

--- ElementOccurence.java
+++ ElementOccurence.java
@@ -1,4 +1,8 @@
 //Program to find out the first occurence of an element and the last occurence of an element in an input array
+//Input: TestCases
+//		 number of array elements
+//		 all the array elements in sorted order
+// 	 	 the number to be searched
 import java.util.*;
 import java.lang.*;
 import java.io.*;
